6ba233043ac66fddbcfc9c8fec4c0c5c2c56ccf2
benjadahl <benjadahl@gmail.com>
2016-09-18 00:39:01 +0200

    Setup basic soup structure

--- BenjaCrawler.py
+++ BenjaCrawler.py
@@ -2,19 +2,40 @@
 
 import wget
 import time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 class site(object):
     def __init__(self, url):
         self.url = url
         self.time = time.time()
+        self.links = []
 
     def dlSite(self):
         try:
-            filename = wget.download(self.url)
+            self.html = urlopen(self.url).read()
         except Exception:
             print("Error while downloading site: " + self.url)
-        #return filename
+
+    def makeSoup(self):
+        try:
+            self.soup = BeautifulSoup(self.html, "lxml")
+        except Exception:
+            print("Error while making soup for: " + self.url)
+
+    def getHref(self):
+        try:
+            self.aTags = self.soup.findAll("a")
+            for element in self.aTags:
+                self.links.append(element["href"])
+        except Exception:
+            print("Error while gettings links for: " + self.url)
+
+
 
 site1 = site("http://www.google.com")
+site1.dlSite()
+site1.makeSoup()
+site1.getHref()
 
-print(site1.dlSite())
+print(site1.links)
